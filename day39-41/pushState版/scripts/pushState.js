7be83c642fc5ffd34fc075951fada61b990f9308
Huyue <824591872@qq.com>
2018-07-04 19:14:56 +0800

    Update pushState.js

--- day39-41/pushState版/scripts/pushState.js
+++ day39-41/pushState版/scripts/pushState.js
@@ -1,11 +1,53 @@
 for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('click', function() {
-        for (var j = 0, arr = []; j < checkboxes.length; j++) {
-            if (j !== 3 && j !== 7 && checkboxes[j].checked) {
-                arr.push(j);
+    checkboxes[i].addEventListener('click', function(e) {
+        if (e.target.id.indexOf('allRegion') + 1) {
+            for (var i = 0; i < 3; i++) {
+                checkboxes[i].checked = ' ';
+            }
+        } else {
+            for (var count1 = 0, i = 0; i < 3; i++) {
+                if (checkboxes[i].checked) {
+                    count1++;
+                }
+            }
+            if (count1 === 0) {
+                e.target.checked = ' ';
+            } else if (count1 === 3) {
+                checkboxes[3].checked = ' ';
+            } else {
+                checkboxes[3].checked = '';
+            }
+        }
+        if (e.target.id.indexOf('allProduct') + 1) {
+            for (var i = 4; i < 8; i++) {
+                checkboxes[i].checked = ' ';
+            }
+        } else {
+            for (var count2 = 0, i = 4; i < 8; i++) {
+                if (checkboxes[i].checked) {
+                    count2++;
+                }
+            }
+            if (count2 === 0) {
+                e.target.checked = ' ';
+            } else if (count2 === 3) {
+                checkboxes[7].checked = ' ';
+            } else {
+                checkboxes[7].checked = '';
             }
         }        
-        history.pushState({checked: arr}, '');
+        
+        var temp = count;
+        count = 0;
+        for (var j = 0, arr = [], count1 = 0, count2 = 0; j < checkboxes.length; j++) {
+            if (j !== 3 && j !== 7 && checkboxes[j].checked) {
+                arr.push(j);
+                count++;
+            }
+        } 
+        if (count !== temp) {
+            history.pushState({checked: arr}, '');
+        }
     });
 }
 
