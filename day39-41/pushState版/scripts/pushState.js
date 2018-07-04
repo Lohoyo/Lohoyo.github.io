bd707e8454cb23b5dae3b0b9a3a5d8eaaaaae0e4
Huyue <824591872@qq.com>
2018-07-04 18:28:00 +0800

    Update pushState.js

--- day39-41/pushState版/scripts/pushState.js
+++ day39-41/pushState版/scripts/pushState.js
@@ -1,7 +1,33 @@
-if (history.state) {
-    for (var i = 0; i < history.state.checked.length; i++) {
-        checkboxes[history.state.checked[i]].setAttribute('checked', '');
-    }    
+for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('click', function() {
+        for (var j = 0, arr = []; j < checkboxes.length; j++) {
+            if (j !== 3 && j !== 7 && checkboxes[j].checked) {
+                arr.push(j);
+            }
+        }        
+        history.pushState({checked: arr}, '');
+    });
 }
 
-// 按钮点击事件放在了checkbox.js里 
+window.addEventListener('popstate', function(e) {
+    if (e.state) {
+        for (var i = 0; i < checkboxes.length; i++) {
+            for (var j = 0; j < e.state.checked.length; j++) {
+                if (i === e.state.checked[j]) {
+                    checkboxes[i].checked = ' ';
+                    break;
+                } else {
+                    checkboxes[i].checked = '';
+                }
+            }
+        }    
+    } else {
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = '';
+        }
+    }
+
+    canvas.width = window.innerWidth / 2 - 16;
+    line.draw();
+    line.setDataAll(getData()[1]);
+});
